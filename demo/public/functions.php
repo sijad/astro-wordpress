c953df606137f47a2aadb08a00426be67914d5df
Sajjad Hashemian <hi@hashemian.me>
2024-01-15 21:46:39 +0330

    update demo

--- demo/public/functions.php
+++ demo/public/functions.php
@@ -6,3 +6,11 @@ function theme_setup(){
 }
 add_action('after_setup_theme','theme_setup');
 
+function astro_wp_demo_init() {
+  register_nav_menus(
+    array(
+        'main-menu'        => __( 'Main menu' ),
+    )
+  );
+}
+add_action( 'init', 'astro_wp_demo_init' );
