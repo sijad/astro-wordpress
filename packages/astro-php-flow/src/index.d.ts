9a95f651f56111be96c55d2ded2c4136931759f1
Sajjad Hashemian <hi@hashemian.me>
2025-05-22 22:19:31 +0330

    add switch & case

--- packages/astro-php-flow/src/index.d.ts
+++ packages/astro-php-flow/src/index.d.ts
@@ -2,6 +2,8 @@ export { default as ForEach } from "./ForEach.astro";
 export { default as If } from "./If.astro";
 export { default as Else } from "./Else.astro";
 export { default as ElseIf } from "./ElseIf.astro";
+export { default as Switch } from "./Switch.astro";
+export { default as Case } from "./Case.astro";
 
 export class HTMLString extends String {
   get [Symbol.toStringTag](): string;
--- packages/astro-php-flow/src/index.js
+++ packages/astro-php-flow/src/index.js
@@ -2,4 +2,6 @@ export { default as ForEach } from "./ForEach.astro";
 export { default as If } from "./If.astro";
 export { default as Else } from "./Else.astro";
 export { default as ElseIf } from "./ElseIf.astro";
+export { default as Switch } from "./Switch.astro";
+export { default as Case } from "./Case.astro";
 export { default as php, HTMLString } from "./php";
