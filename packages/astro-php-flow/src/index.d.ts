6fb61684bd3459c4742c04dacd39a3e6a07615d6
Sajjad Hashemian <hi@hashemian.me>
2025-11-01 17:18:38 +0330

    add While

--- packages/astro-php-flow/src/index.d.ts
+++ packages/astro-php-flow/src/index.d.ts
@@ -6,6 +6,7 @@ export { default as Switch } from "./Switch.astro";
 export { default as Case } from "./Case.astro";
 export { default as Break } from "./Break.astro";
 export { default as Continue } from "./Continue.astro";
+export { default as While } from "./While.astro";
 
 export class HTMLString extends String {
   get [Symbol.toStringTag](): string;
